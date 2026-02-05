94548916238580a5b68e3dfdf121fcca7843b992
haku <rootlogic7@proton.me>
2026-02-05 16:22:48 +0100

    feat(hyprland): Adding more keybindings

--- home.nix
+++ home.nix
@@ -77,6 +77,18 @@
         "$mod, right, movefocus, r"
         "$mod, up, movefocus, u"
         "$mod, down, movefocus, d"
+
+	# Workspaces
+	"$mod, 1, workspace, 1"
+	"$mod, 2, workspace, 2"
+	"$mod, 3, workspace, 3"
+	"$mod, 4, workspace, 4"
+	"$mod, 5, workspace, 5"
+	"$mod, 6, workspace, 6"
+	"$mod, 7, workspace, 7"
+	"$mod, 8, workspace, 8"
+	"$mod, 9, workspace, 9"
+	"$mod, 0, workspace, 10"
       ];
 
       # Design (CachyOS / Hyprland Standard Stil)
