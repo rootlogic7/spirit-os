218137ce5bf26300c493e34cc21f1b087849f2c3
Haku <rootlogic7@proton.me>
2026-02-22 11:58:45 +0100

    feat: user haku uses sops for password and github ssh key

--- modules/users/haku.nix
+++ modules/users/haku.nix
@@ -4,8 +4,8 @@
   users.users.haku = { 
     isNormalUser = true;
     description = "Haku";
-    # hashedPasswordFile = config.sops.secrets."haku-password".path;
-    initialPassword = "haku";
+    hashedPasswordFile = config.sops.secrets."haku-password".path;
+    # initialPassword = "haku";
     extraGroups = [ "networkmanager" "wheel" "video" "gamemode" ];
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC6FGcKzp9lwFTWQXNNLB1xJC07rTWJeK2GN0J9mcjqg rootlogic7@proton.me" ];
@@ -13,4 +13,11 @@
 
   # Zsh auf Systemebene aktivieren, damit Login funktioniert
   programs.zsh.enable = true;
+
+  # --- SOPS USER SECRETS ---
+  sops.secrets."github-ssh-key" = {
+    path = "/home/haku/.ssh/id_ed25519";
+    owner = "config.users.users.haku.name";
+    mode = "0400";
+  };
 }
